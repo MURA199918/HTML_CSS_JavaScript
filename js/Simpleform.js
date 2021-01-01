67430d6202d01e1ae1da92a378c61f43609ba396
MURA199918 <muralisaranr18@gmail.com>
2021-01-01 11:31:23 +0530

    [Murali]Refactored to validate Email using regex

--- js/Simpleform.js
+++ js/Simpleform.js
@@ -1,9 +1,19 @@
+//Validate Name
 const text = document.querySelector("#text");
 const textError = document.querySelector(".text-error");
 text.addEventListener("input", function () {
     let nameRegex = RegExp("^[A-Z]{1}[a-z]{2,}$");
     if (nameRegex.test(text.value)) textError.textContent = "";
     else textError.textContent = "Name is Incorrect";
+});
+
+//Validate Email
+const email = document.querySelector("#email");
+const emailError = document.querySelector(".email-error");
+email.addEventListener("input",function(){
+    let emailRegex = RegExp("^[a-zA-Z0-9+_-]+([.][a-zA-Z0-9]+)*@([a-zA-Z0-9]+)([.][a-z]+)?[.][a-z]{2,}$");
+    if(emailRegex.test(email.value)) emailError.textContent="";
+    else emailError.textContent = "Email is Incorrect";
 });
 
 const salary = document.querySelector('#salary');
