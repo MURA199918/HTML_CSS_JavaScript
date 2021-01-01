18f2d95af1f78c226fc6b568819f4e76eefff3c5
MURA199918 <muralisaranr18@gmail.com>
2021-01-01 11:36:28 +0530

    [Murali]Refactored to validate Telephone using regex

--- js/Simpleform.js
+++ js/Simpleform.js
@@ -16,6 +16,15 @@ email.addEventListener("input",function(){
     else emailError.textContent = "Email is Incorrect";
 });
 
+//Validate Telephone number
+const tel = document.querySelector("#tel");
+const telError = document.querySelector(".tel-error");
+tel.addEventListener("input",function(){
+    let telRegex = RegExp("(([0-9]{2})?)[ ][0-9]{10}");
+    if(telRegex.test(tel.value)) telError.textContent="";
+    else telError.textContent = "Telephone is Incorrect";
+});
+
 const salary = document.querySelector('#salary');
 const output = document.querySelector('.salary-output');
 output.textContent = salary.value;
