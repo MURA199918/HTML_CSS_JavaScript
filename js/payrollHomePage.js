fa97c1b675f618b3298645a4325c1ea9fb46efac
MURA199918 <muralisaranr18@gmail.com>
2021-01-06 19:19:47 +0530

    [Murali]Refactored to display employee details from local storage

--- js/payrollHomePage.js
+++ js/payrollHomePage.js
@@ -1,13 +1,23 @@
-
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', (event) => {
+    empPayrollList = getEmployeePayrollDataFromStorage();
+    document.querySelector(".emp-count").textContent = empPayrollList.length;
     createInnerHtml();
+    localStorage.removeItem('editEmp');
 });
+
+const getEmployeePayrollDataFromStorage = () => {
+    return localStorage.getItem('EmployeePayrollList') ?
+                        JSON.parse(localStorage.getItem('EmployeePayrollList')) : [];
+}
 
 const createInnerHtml = () => {
     const headerHtml = "<th></th><th>Name</th><th>Gender</th><th>Department</th>"+
                        "<th>Salary</th><th>Start Date</th><th>Actions</th>";
-    let empPayrollData = createEmployeePayrollJSON()[0];
-    const innerHtml = `${headerHtml}
+    if(empPayrollList.length == 0) return;
+    let innerHtml = `${headerHtml}`;
+    for(const empPayrollData of empPayrollList){
+        innerHtml = `${innerHtml}
         <tr>
             <td><img class="profile" src="${empPayrollData._profilePic}" alt=""></td>
             <td>${empPayrollData._name}</td>
@@ -23,6 +33,7 @@ const createInnerHtml = () => {
             </td>
         </tr>
     `;
+    }
     document.querySelector('#table-display').innerHTML = innerHtml;
 }
 
